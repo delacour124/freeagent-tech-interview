85f2c038206dae8bb0be0ca3b423c70d66ef9fe8
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-10-21 14:30:57 -0700

    received ditto name and image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  // create state
+  const [pokemon, setPokemon] = useState();
+
+  // fetch data
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+      const pokemon = await response.json();
+      setPokemon(pokemon);
+    }
+
+    fetchData();
+  }, [])
+
+  console.log(pokemon);
+  
+
+  if (!pokemon) return null;
+
+  const pokemonName = pokemon.species.name;
+  const pokemonImgURL = pokemon.sprites.front_default;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{pokemonName}</h1>
+      <img src={pokemonImgURL} alt='ditto'/>
     </div>
-  );
+  )
 }
 
 export default App;
